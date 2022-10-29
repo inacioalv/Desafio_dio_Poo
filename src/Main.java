1eefa5fb30ef3fe01506954f1d53792f3b29b741
inacioalv <51245154+inacioalv@users.noreply.github.com>
2022-10-29 11:40:02 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import br.com.inacio.dominio.Curso;
+import br.com.inacio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso = new Curso();
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Primeiro curso");
+        curso.setCargaHorario(1);
+
+        System.out.println(curso);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("Descrição java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+
+
     }
 }
